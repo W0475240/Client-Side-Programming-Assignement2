ba98a19e0f36793c6a0659e31e2aebe51fb16148
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-10-09 15:53:50 -0300

    finall version

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ const url = 'https://dog-api.kinduff.com/api/facts';
 // sending request
 fetch(url).then(async (response) => 
 {
-  
+  const data = await response.json();
+  console.log(data);
+  success_status = data.success ? "successful" : "unsuccessful";
+  document.write("<h2> Did you know that " + data.facts[0] + " (api call was " + success_status + ") </h2>");
 });
