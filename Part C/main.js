c8b5e806760cc98c4ca037d3b1975f46fdf817ef
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-10-10 00:37:34 -0300

    same suit function

--- Part C/main.js
+++ Part C/main.js
@@ -1,1 +1,18 @@
 const url = 'https://deckofcardsapi.com/api/deck/new/shuffle/';
+
+
+
+function is_same_suite(cards)
+{
+  var first_card_suite = cards[0].code.split("")[1]; 
+
+  for (var i = 1; i < cards.length; i++)
+  {
+    if (cards[i].code.split("")[1] != first_card_suite)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
